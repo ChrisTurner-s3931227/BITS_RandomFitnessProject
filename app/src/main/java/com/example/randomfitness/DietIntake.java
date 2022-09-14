e72cf8f07f6f7a5e955c92e01e7ae372558aa1ad
LachCasey <s3721527@student.rmit.edu.au>
2022-09-15 00:10:37 +1000

    Fixed and Added the basic diet functions
    Diet functions now work properly unlike before and can be accessed from the main menu, SQLite still yet to be added.

---
+++ app/src/main/java/com/example/randomfitness/DietIntake.java
@@ -0,0 +1,56 @@
+package com.example.randomfitness;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class DietIntake extends AppCompatActivity {
+
+    private EditText breakfast;
+    private EditText lunch;
+    private EditText dinner;
+    private EditText snacks;
+
+    private TextView intake;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diet_intake);
+
+        breakfast = (EditText) findViewById(R.id.breakfasttext);
+        lunch = (EditText) findViewById(R.id.lunchtext);
+        dinner = (EditText) findViewById(R.id.dinnertext);
+        snacks = (EditText) findViewById(R.id.snacktext);
+
+        intake = (TextView) findViewById(R.id.dailytotaltextView);
+
+        Button logintake = (Button) findViewById(R.id.intakebutton);
+
+        logintake.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dailyintake();
+            }
+        });
+    }
+
+    private void dailyintake() {
+        double breakfastintake = Double.valueOf(breakfast.getText().toString());
+        double lunchintake = Double.valueOf(lunch.getText().toString());
+        double dinnerintake = Double.valueOf(dinner.getText().toString());
+        double snackintake = Double.valueOf(snacks.getText().toString());
+        double result = breakfastintake + lunchintake + dinnerintake + snackintake;
+        intake.setText("Daily Intake: " + Math.round(result));
+    }
+
+    public void goToDietSettings (View view){
+        Intent intent = new Intent (this, DietSettings.class);
+        startActivity(intent);
+    }
+}
